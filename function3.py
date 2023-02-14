adec1613ca78e5386cad20aaa498470a4e516dad
pwknd <pwknd@naver.com>
2023-02-14 15:16:12 +0900

    함수(인자처리 정리)

---
+++ function3.py
@@ -0,0 +1,44 @@
+# function3.py
+# 기본값 세팅
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드인자 방식
+def connectURI(server, port):
+    starURL = "https://" + server + ":" + port
+    return starURL
+
+#호출
+print(connectURI("credu.com","80"))
+print(connectURI(port="8080", server="credu.com"))
+
+
+#가변인자(가변적인 상황처리)
+def union(*ar):
+    result = []
+    for items in ar:
+        for x in items:
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출
+print( union("ham","egg"))
+print( union("ham","egg","spam"))
+
+
+#정의되지 않은 인자 처리
+def userURILBuilder(server, port, **user):
+    starURL = "https://" + server + ":" + port + "/?"
+    for key in user.keys():
+        starURL += key + "=" + user[key] + "&"
+    return starURL
+
+#호출 
+print( userURILBuilder("credu.com","80", id="kim", passwd="1234"))
+print( userURILBuilder("credu.com","80", id="kim", passwd="1234", name="mike"))
