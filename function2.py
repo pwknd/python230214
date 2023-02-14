77f2d439f56b8330b717be564345f7359d1c47e5
pwknd <pwknd@naver.com>
2023-02-14 14:07:18 +0900

    디버깅 연습

--- function2.py
+++ function2.py
@@ -33,3 +33,17 @@ def func2(a):
 
 #호출 
 print(func2(1))
+
+
+
+# 디버깅 연습용 교집한 리턴 함수 
+def intersect(prelist,postlist):
+    #지역변수에 교집합 문자 모으기 
+    result = [] 
+    for x in prelist:
+        # x에 ham 중 a가 있고 result에 없으면 a를 추가해라 
+        if x in postlist and x not in result: 
+            result.append(x)
+    return result
+# 호출 
+print(intersect("Ham","Spam"))
